0263a05259cfa986ad13ccd5d8e2d6a779a5f161
HJG827 <hanjingyeong97@gmail.com>
2025-12-13 23:34:24 +0900

    [Silver V] Title: 집합, Time: 5308 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/11723. 집합/집합.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+M = int(input())
+S = set()
+
+for _ in range(M):
+    command = list(input().split())
+    if command[0] == "add":
+        S.add(int(command[1]))
+    elif command[0] == "remove":
+        S.discard(int(command[1]))
+    elif command[0] == "check":
+        if int(command[1]) in S:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == "toggle":
+        if int(command[1]) in S:
+            S.discard(int(command[1]))
+        else:
+            S.add(int(command[1]))
+    elif command[0] == "all":
+        S = set(i for i in range(1, 21))
+    elif command[0] == "empty":
+        S = set()
