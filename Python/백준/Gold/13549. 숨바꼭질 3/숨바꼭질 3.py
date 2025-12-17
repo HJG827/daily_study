496f299f12a8583034199a1f6acf7bb04f0fa3f0
HJG827 <hanjingyeong97@gmail.com>
2025-12-17 21:57:06 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 124 ms, Memory: 35292 KB -BaekjoonHub

---
+++ Python/백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,37 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+if N >= K:
+    print(N - K)
+
+else:
+    MAX = 100001
+    dist = [MAX] * MAX
+    q = deque()
+    q.append(N)
+    dist[N] = 0
+    while q:
+        now = q.popleft()
+
+        if now == K:
+            break
+        
+        for next in (now + 1, now - 1, now * 2):
+            if (0 <= next < 100001):
+                if (next == now * 2
+                    and dist[next] > dist[now]):
+                    dist[next] = dist[now]
+                    q.appendleft(next)
+
+                elif (
+                    next != now * 2
+                    and dist[next] > dist[now] + 1
+                ):
+                    dist[next] = dist[now] + 1
+                    q.append(next)
+                continue
+
+    print(dist[K])
