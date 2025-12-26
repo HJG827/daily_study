919393671554aeeb13843ca6d1ef599c78e17c0a
HJG827 <hanjingyeong97@gmail.com>
2025-12-26 15:38:15 +0900

    [Silver III] Title: 이친수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/2193. 이친수/이친수.py
@@ -0,0 +1,17 @@
+N = int(input())
+
+if N < 4:
+    dp = [0] * 4
+else:
+    dp = [0] * (N + 1)
+
+dp[1] = 1
+dp[2] = 1
+dp[3] = 2
+
+if N < 4:
+    print(dp[N])
+else:
+    for i in range(4, N + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    print(dp[N])
