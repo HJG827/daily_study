3a2eb1717e43f391ffcdf0ec905a91f6b636785c
HJG827 <hanjingyeong97@gmail.com>
2026-01-27 19:39:54 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1629. 곱셈/곱셈.py
+++ Python/백준/Silver/1629. 곱셈/곱셈.py
@@ -3,4 +3,16 @@ input = sys.stdin.readline
 
 A, B, C = map(int, input().split())
 
-print(pow(A, B, C))
+def mod_pow(a, b, m):
+    if b == 0:
+        return 1
+    if b == 1:
+        return a % m
+    half = mod_pow(a, b // 2, m)
+    half = (half * half) % m
+    if b % 2 == 0:
+        return half
+    else:
+        return (half * (a % m)) % m
+    
+print(mod_pow(A, B, C))
