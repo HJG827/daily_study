555675aec404f1e6bd29cee9827ab11dc944342d
HJG827 <hanjingyeong97@gmail.com>
2026-04-05 04:00:24 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 332 ms, Memory: 60360 KB -BaekjoonHub

---
+++ Python/백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+dots = [list(map(int, input().split())) for _ in range(N)]
+
+dots.sort(key=lambda x:(x[1], x[0]))
+
+for x, y in dots:
+    print(x, y)
