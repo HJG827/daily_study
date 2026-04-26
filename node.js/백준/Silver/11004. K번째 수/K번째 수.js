e98348c6d06c06ef25daab507116ab813431a21d
HJG827 <hanjingyeong97@gmail.com>
2026-04-26 16:59:46 +0900

    [Silver V] Title: K번째 수, Time: 4208 ms, Memory: 567412 KB -BaekjoonHub

---
+++ node.js/백준/Silver/11004. K번째 수/K번째 수.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+const input = fs.readFileSync(0, "utf8").trim().split("\n");
+
+const [N, K] = input[0].split(" ").map(Number);
+const arr = input[1].split(" ").map(Number);
+
+arr.sort((a, b) => a - b);
+
+console.log(arr[K - 1]);
