32b07294598fbbd822dab0efab306606dd7b2ee8
HJG827 <hanjingyeong97@gmail.com>
2026-05-14 00:09:33 +0900

    [level 2] Title: 숫자의 표현, Time: 1.45 ms, Memory: 35 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,27 @@
+function solution(n) {
+    var answer = 0;
+    
+    let left = 1;
+    let right = 1;
+    let sum = 1;
+    
+    while (left <= n) {
+        if (sum == n) {
+            answer += 1;
+            sum -= left
+            left++
+        } else if (sum < n) {
+            right++
+            sum += right
+        } else {
+            sum -= left
+            left++
+        }
+    
+    }
+    
+    
+    return answer;
+    
+    
+}
